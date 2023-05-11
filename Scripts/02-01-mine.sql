e475f9c141cffb53ff7b1f21a2a94c2e25c9d78e
Chase Meyer <chase.e.meyer@gmail.com>
2023-05-11 21:56:41 +0000

    02-01-mine complete

--- Scripts/02-01-mine.sql
+++ Scripts/02-01-mine.sql
@@ -1,1 +1,12 @@
 SELECT sql FROM sqlite_schema;
+
+SELECT 
+  e.employeeId AS 'EmployeeID',
+  e.firstName AS 'First Name',
+  e.lastName AS 'Last Name',
+  COUNT(*) AS 'Number of Sales'
+FROM employee e 
+JOIN sales s ON
+  e.employeeId = s.employeeId
+GROUP BY e.employeeId
+ORDER BY e.employeeId;
